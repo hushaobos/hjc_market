cbe7da6f6dd0679fce7b7e8846cd5951d7c8ec7b
WIN-CS60JJEQBU5\Administrator <932972523@qq.com>
2019-09-23 20:55:35 +0800

    加入导航栏

--- src/home/Home.js
+++ src/home/Home.js
@@ -2,12 +2,14 @@ import React, { PureComponent, Fragment } from 'react';
 import HomeBanner from './Banner';
 import Cup from './cup/Cup';
 import MainNav from './mainnav/MainNav';
+import TitleNav from './TitleNav';
 
 class Home extends PureComponent{
   render(){
     return <Fragment>
               <HomeBanner></HomeBanner>
               <Cup></Cup>
+              <TitleNav></TitleNav>
               <MainNav></MainNav>
           </Fragment>;
   }
---
+++ src/home/TitleNav.js
@@ -0,0 +1,45 @@
+import React,{PureComponent} from 'react';
+import './TitleNav.less';
+
+class TitleNav extends PureComponent{
+  state = {
+    navs:[
+      {
+        title:'装修报价',
+        link:''
+      },
+      {
+        title:'户型方案',
+        link:''
+      },
+      {
+        title:'小窝社区',
+        link:''
+      },
+      {
+        title:'窝立方',
+        link:'http://www.wlf360.com'
+      }
+    ]
+  };
+
+  render = () =>{
+    const {navs} = this.state;
+    return (
+      <div className='titles-nav'>
+        <h2 className='g-title'>家居导购</h2>
+        <ul className='wlf-nav'>
+          {
+            navs.map((value,index) => (
+              <li key={index}>
+                <a href={value.link}>{value.title}</a>
+              </li>
+            ))
+          }
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default TitleNav;
